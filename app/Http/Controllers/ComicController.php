13df127a25dc1500b427a5e29b37ad4200c193fd
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-02-19 10:40:04 +0100

    [UPDATE] migration table and seeding

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Comic;
 use Illuminate\Http\Request;
 
+
+
 class ComicController extends Controller
 {
     /**
--- database/migrations/2024_02_19_085344_create_comics_table.php
+++ database/migrations/2024_02_19_085344_create_comics_table.php
@@ -4,6 +4,8 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+use App\Models\Comic;
+
 return new class extends Migration
 {
     /**
@@ -15,6 +17,13 @@ return new class extends Migration
     {
         Schema::create('comics', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('description');
+            $table->text('thumb');
+            $table->string('price');
+            $table->string('series');
+            $table->date('sale_date');
+            $table->string('type');
             $table->timestamps();
         });
     }
